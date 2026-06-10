a9df45871e746f4cc46494c09d9aadcc03851a49
BandaVandhana <bandavandhana26@gmail.com>
2026-06-10 17:27:57 +0530

    Update Gemini API key

--- src/services/geminiService.js
+++ src/services/geminiService.js
@@ -1,7 +1,7 @@
+
 const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY
 
-const GEMINI_URL =
-  `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${GEMINI_API_KEY}`
+const GEMINI_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${GEMINI_API_KEY}`
 
 /* ---------------- SAFE JSON PARSER ---------------- */
 function extractJson(text) {
